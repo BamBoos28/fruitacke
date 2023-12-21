c53e9cf901c6efd846a0b45f71c86fe17d5ce0de
BamBoos28 <119471667+BamBoos28@users.noreply.github.com>
2023-12-21 21:15:46 +0700

    minor

--- catalog.php
+++ catalog.php
@@ -34,7 +34,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 <?php include("./comp/navbar.php"); ?>
 
 <!-- Bestsaler Product Start -->
-<div class="container-fluid py-5">
+<div class="container-fluid" style="margin:8em 0 2em 0 ;">
     <div class="container py-5">
         <div class="text-center mx-auto mb-5" style="max-width: 700px;">
             <h1 class="display-4">Bestseller Products</h1>
